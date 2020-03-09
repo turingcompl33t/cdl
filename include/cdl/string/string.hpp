90fee9c2845e0bdf6b53971e7e9604256c433c3d
kyledotterrer <kdotterrer@gmail.com>
2020-03-08 20:32:20 -0400

    basic copy and move semantics for mutable string

--- include/cdl/string/string.hpp
+++ include/cdl/string/string.hpp
@@ -20,54 +20,63 @@ namespace cdl::string
     class basic_string
     {   
         Char*       m_buffer;
-        std::size_t m_size;
+        std::size_t m_count;     // count of characters in string
+        std::size_t m_capacity;  // size of underlying buffer (in characters)
 
     public:
         basic_string()
             : m_buffer{ nullptr }
-            , m_size{ 0 }
+            , m_count{ 0 }
+            , m_capacity{ 0 }
         {}
 
         basic_string(Char const* str)
         {
-            auto len = string_length(str);
+            auto len = detail::string_length(str);
             
             m_buffer = new Char[len + 1];
             
-            string_copy(m_buffer, str, len);
+            detail::string_copy(m_buffer, str, len);
             m_buffer[len] = '\0';
 
-            m_size = len;
+            m_count    = len;
+            m_capacity = len + 1;
         }
 
         basic_string(basic_string const& other)
-            : m_buffer{ new char[other.m_size] }
-            , m_size{ other.m_size }
+            : m_buffer{ new Char[other.m_capacity] }
+            , m_count{ other.m_count }
+            , m_capacity{ other.m_capacity }
         {
-            string_copy(m_buffer, other.m_buffer, m_size);
+            detail::string_copy(m_buffer, other.m_buffer, m_count);
+            m_buffer[m_count] = '\0';
         }
 
         basic_string& operator=(basic_string const& rhs)
         {
             if (this == &rhs) return *this;
             
-            auto new_buffer = new char[rhs.m_size];
+            auto new_buffer = new char[rhs.m_capacity];
             delete[] m_buffer;
             
-            m_buffer = new_buffer;
-            m_size   = rhs.m_size;
+            m_buffer   = new_buffer;
+            m_count    = rhs.m_count;
+            m_capacity = rhs.m_capacity;
             
-            string_copy(m_buffer, rhs.m_buffer, m_size);
+            detail::string_copy(m_buffer, rhs.m_buffer, m_count);
+            m_buffer[m_count] = '\0';
             
             return *this;
         }
 
         basic_string(basic_string&& other) noexcept
             : m_buffer{ other.m_buffer }
-            , m_size{ other.m_size }
+            , m_count{ other.m_count }
+            , m_capacity{ other.m_capacity }
         {
-            other.m_buffer = nullptr;
-            other.m_size   = 0;
+            other.m_buffer   = nullptr;
+            other.m_count    = 0;
+            other.m_capacity = 0;
         }
 
         basic_string& operator=(basic_string&& rhs) noexcept
@@ -76,11 +85,13 @@ namespace cdl::string
             
             delete[] m_buffer;
 
-            m_buffer = rhs.m_buffer;
-            m_size   = rhs.m_size;
+            m_buffer   = rhs.m_buffer;
+            m_count    = rhs.m_count;
+            m_capacity = rhs.m_capacity;
 
-            rhs.m_buffer = nullptr;
-            rhs.m_size   = 0;
+            rhs.m_buffer   = nullptr;
+            rhs.m_count    = 0;
+            rhs.m_capacity = 0;
 
             return *this;
         }
@@ -93,6 +104,9 @@ namespace cdl::string
         std::size_t size() const noexcept;
         std::size_t length() const noexcept;
         bool empty() const noexcept;
+
+        bool is_equal(basic_string const& other);
+        bool is_equal(Char const* str);
     };
 
     using string  = basic_string<char>;
@@ -101,18 +115,34 @@ namespace cdl::string
     template <typename Char>
     std::size_t basic_string<Char>::size() const noexcept
     {
-        return m_size;
+        return m_count;
     }
 
     template <typename Char>
     std::size_t basic_string<Char>::length() const noexcept
     {
-        return m_size;
+        return m_count;
     }
 
     template<typename Char>
     bool basic_string<Char>::empty() const noexcept
     {
-        return (m_size == 0);
+        return (m_count == 0);
+    }
+
+    template <typename Char>
+    bool basic_string<Char>::is_equal(
+        basic_string<Char> const& other
+        )
+    {
+        return detail::string_compare(m_buffer, other.m_buffer);
+    }
+
+    template <typename Char>
+    bool basic_string<Char>::is_equal(
+        Char const* str
+    )
+    {
+        return detail::string_compare(m_buffer, str);
     }
 }
--- test/string/basic_string.cpp
+++ test/string/basic_string.cpp
@@ -22,6 +22,46 @@ TEST_CASE("cdl::string::basic_string<char> semantics behave as expected")
         REQUIRE_FALSE(s.empty());
         REQUIRE(s.length() == 5);
     }
+
+    SECTION("copy construction")
+    {
+        auto src = string{"Hello"};
+        auto dst = string{src};
+
+        REQUIRE_FALSE(dst.empty());
+        REQUIRE(dst.length() == 5);
+        REQUIRE(dst.is_equal(src));
+    }
+
+    SECTION("copy assignment")
+    {
+        auto src = string{"hello"};
+        auto dst = src;
+
+        REQUIRE_FALSE(dst.empty());
+        REQUIRE(dst.length() == 5);
+        REQUIRE(dst.is_equal(src));
+    }
+
+    SECTION("move construction")
+    {
+        auto src = string{"hello"};
+        auto dst{std::move(src)};
+
+        REQUIRE_FALSE(dst.empty());
+        REQUIRE(dst.length() == 5);
+        REQUIRE(dst.is_equal("hello"));
+    }
+
+    SECTION("move assignment")
+    {
+        auto src = string{"hello"};
+        auto dst = std::move(src);
+
+        REQUIRE_FALSE(dst.empty());
+        REQUIRE(dst.length() == 5);
+        REQUIRE(dst.is_equal("hello"));
+    }
 }
 
 TEST_CASE("cdl::string::basic_string<wchar_t> semantics behave as expected")
@@ -41,6 +81,46 @@ TEST_CASE("cdl::string::basic_string<wchar_t> semantics behave as expected")
         
         REQUIRE_FALSE(ws.empty());
         REQUIRE(ws.length() == 5);
+    }
+
+    SECTION("copy construction")
+    {
+        auto src = wstring{L"Hello"};
+        auto dst = wstring{src};
+
+        REQUIRE_FALSE(dst.empty());
+        REQUIRE(dst.length() == 5);
+        REQUIRE(dst.is_equal(src));
+    }
+
+    SECTION("copy assignment")
+    {
+        auto src = wstring{L"hello"};
+        auto dst = src;
+
+        REQUIRE_FALSE(dst.empty());
+        REQUIRE(dst.length() == 5);
+        REQUIRE(dst.is_equal(src));
+    }
+
+    SECTION("move construction")
+    {
+        auto src = wstring{L"hello"};
+        auto dst{std::move(src)};
+
+        REQUIRE_FALSE(dst.empty());
+        REQUIRE(dst.length() == 5);
+        REQUIRE(dst.is_equal(L"hello"));
+    }
+
+    SECTION("move assignment")
+    {
+        auto src = wstring{L"hello"};
+        auto dst = std::move(src);
+
+        REQUIRE_FALSE(dst.empty());
+        REQUIRE(dst.length() == 5);
+        REQUIRE(dst.is_equal(L"hello"));
     }
 }
 
